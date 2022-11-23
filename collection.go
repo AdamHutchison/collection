cc496b72871b1ba7568e95d1c6cb9df16653b661
Adam Hutchison <adam.hutchison@gophr.it>
2022-11-23 20:30:57 +0000

    Adds All method

--- collection.go
+++ collection.go
@@ -1,1 +1,9 @@
 package collection
+
+type Collection[T any] struct {
+	set []T
+}
+
+func (c *Collection[T]) All() []T {
+	return c.set
+}
--- collection_test.go
+++ collection_test.go
@@ -1,1 +1,18 @@
 package collection
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestCanGetSet(t *testing.T) {
+	set := []int{1, 2, 3, 4}
+	c := Collection[int]{set: set}
+
+	want := set
+	got := c.All()
+
+	if reflect.DeepEqual(want, got) != true {
+		t.Errorf("Wanted %v, Got %v", want, got)
+	}
+}
