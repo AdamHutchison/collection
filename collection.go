1dac2d70c07ee5f38260a04c987cc0ec613ad6eb
Adam Hutchison <adam.hutchison@gophr.it>
2022-11-25 08:23:59 +0000

    Adds Append method to collection

--- collection.go
+++ collection.go
@@ -7,3 +7,8 @@ type Collection[T any] struct {
 func (c *Collection[T]) All() []T {
 	return c.set
 }
+
+func (c *Collection[T]) Append(item T) Collection[T] {
+	c.set = append(c.set, item)
+	return *c
+}
--- collection_test.go
+++ collection_test.go
@@ -5,7 +5,7 @@ import (
 	"testing"
 )
 
-func TestCanGetSet(t *testing.T) {
+func TestCanGetAllItems(t *testing.T) {
 	set := []int{1, 2, 3, 4}
 	c := Collection[int]{set: set}
 
@@ -14,5 +14,23 @@ func TestCanGetSet(t *testing.T) {
 
 	if reflect.DeepEqual(want, got) != true {
 		t.Errorf("Wanted %v, Got %v", want, got)
+	}
+}
+
+func TestCanAppendItemToCollection(t *testing.T) {
+	c := Collection[string]{}
+	c.Append("Test String")
+
+	setLenth := len(c.All())
+
+	if len(c.All()) != 1 {
+		t.Errorf("Incorrect slice count when appending to collection. Wanted 1, Got %v", setLenth)
+	}
+
+	want := "Test String"
+	got := c.All()[0]
+
+	if want != got {
+		t.Errorf("Want: %v, Got: %v", want, got)
 	}
 }
