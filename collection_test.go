d2c31668b76028600719cc12be2d296e265b292d
Adam Hutchison <adam.hutchison@gophr.it>
2022-11-25 11:36:28 +0000

    Adds count method

--- collection_test.go
+++ collection_test.go
@@ -13,7 +13,7 @@ func TestCanGetAllItems(t *testing.T) {
 	got := c.All()
 
 	if reflect.DeepEqual(want, got) != true {
-		t.Errorf("Wanted %v, Got %v", want, got)
+		t.Errorf("Wanted: %v, Got: %v", want, got)
 	}
 }
 
@@ -24,7 +24,7 @@ func TestCanAppendItemToCollection(t *testing.T) {
 	setLenth := len(c.All())
 
 	if len(c.All()) != 1 {
-		t.Errorf("Incorrect slice count when appending to collection. Wanted 1, Got %v", setLenth)
+		t.Errorf("Incorrect slice count when appending to collection. Wanted: 1, Got: %v", setLenth)
 	}
 
 	want := "Test String"
@@ -36,8 +36,8 @@ func TestCanAppendItemToCollection(t *testing.T) {
 }
 
 func TestCanPopItemFromCollection(t *testing.T) {
-	c := Collection[int] {
-		set: []int {1,2,3,4,5},
+	c := Collection[int]{
+		set: []int{1, 2, 3, 4, 5},
 	}
 
 	// Test the poped item is the last value in set
@@ -53,9 +53,51 @@ func TestCanPopItemFromCollection(t *testing.T) {
 	}
 
 	// Test the item was removed from the collection
-	valueWasRemovedFromCollection := reflect.DeepEqual([]int {1,2,3,4}, c.All())
+	valueWasRemovedFromCollection := reflect.DeepEqual([]int{1, 2, 3, 4}, c.All())
 
 	if !valueWasRemovedFromCollection {
 		t.Errorf("Failed to assert that the value was removed from the collection")
+	}
+}
+
+func TestCanSuffleCollection(t *testing.T) {
+	c := Collection[int] {
+		set: []int{1,2,3,4,5},
+	}
+
+	got := c.Shuffle()
+	want := 1
+
+	if want != got {
+		t.Errorf("Shuffle() did not return the expected value. Wanted: %v, Got: %v", want, got)
+	}
+
+	newCollectionLength := len(c.All())
+
+	if newCollectionLength != 4 {
+		t.Errorf("Length of Collection after Shuffle() is not correct. Wanted: %v, Got: %v", 4, newCollectionLength)
+	}
+
+	valueWasRemovedFromCollection := reflect.DeepEqual([]int{2, 3, 4, 5}, c.All())
+
+	if !valueWasRemovedFromCollection {
+		t.Errorf("Failed asserting that 1st item was removed from Collection")
+	}
+}
+
+func TestCanCountCollection(t *testing.T) {
+	c := Collection[string]{
+		set: []string{
+			"item 1",
+			"item 2",
+			"item 3",
+		},
+	}
+
+	want := 3
+	got := c.Count()
+
+	if want != got {
+		t.Errorf("Wanted: %v, Got: %v", want, got)
 	}
 }
